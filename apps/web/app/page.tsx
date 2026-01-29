cf276187b8cc1082e26b0ca8efa21a3f10cd2aec
xwaynex <waynexaint@gmail.com>
2026-01-29 18:22:01 +0000

    feat: enhance CompanionCard component with props for dynamic content rendering

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -7,9 +7,9 @@ const Page = () => {
     <main>
       <h1>Popular Companions</h1>
       <section className="home-section">
-        <CompanionCard />
-        <CompanionCard />
-        <CompanionCard />
+        <CompanionCard id="123" name="Neura the Brainy Explorer" topic="Neural Network of the Brain" subject='science' duration={45} color="#ffda6e" />
+        <CompanionCard id="456" name="Countsy the Number Wizard" topic="Derivatives & Integrals" subject='Maths' duration={30} color="#e5d0ff" />
+        <CompanionCard id="789" name="Verba the Vocabulary Builder" topic="English Literature " subject='science' duration={30} color="#BDE7FF" />
       </section>
 
       <section className="home-section">
--- apps/web/components/CompanionCard.tsx
+++ apps/web/components/CompanionCard.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
-const CompanionCard = () => {
-  return (
-    <div>CompanionCard</div>
-  )
+interface CompanionCardProps {
+  id: string;
+  name: string;
+  topic: string;
+  subject: string;
+  duration: number;
+  color: string;
 }
 
-export default CompanionCard
+const CompanionCard = ({
+  id,
+  name,
+  topic,
+  subject,
+  duration,
+  color,
+}: CompanionCardProps) => {
+  return (
+    <article className="companion-card" style={{ background: color }}>
+      <div className="flex justify-between items-center">
+        <div className="subject-badge">{subject}</div>
+        <button className="companion-bookmark">
+          <Image
+            src="/icons/bookmark.svg"
+            alt="Bookmark Icon"
+            width={12.5}
+            height={15}
+          />
+        </button>
+      </div>
+
+      <h2 className="text-2xl font-bold">{name}</h2>
+      <p className="text-sm">{topic}</p>
+      <div className="flex items-center gap-2">
+        <Image src="/icons/clock.svg" alt="Clock Icon" width={13.5} height={13.5} />
+        <p className="text-sm">{duration} minutes</p>
+      </div>
+
+      <Link href={`/companions/${id}`} className="w-full">
+      <button className="btn-primary w-full justify-center">
+        Launch Lesson
+      </button>
+      </Link>
+    </article>
+  );
+};
+
+export default CompanionCard;
