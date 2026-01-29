070fa4b113099e48834e73adafc34141298c838d
xwaynex <waynexaint@gmail.com>
2026-01-29 17:17:37 +0000

    feat: add Navbar and NavItems components for improved navigation

---
+++ apps/web/components/NavItems.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const navItems = [
+  { label: "Home", href: "/" },
+  { label: "Companions", href: "/companions" },
+  { label: "My journey", href: "/my-journey" },
+];
+
+const NavItems = () => {
+  const pathname = usePathname();
+
+  return (
+    <nav className="flex items-center gap-4">
+      {navItems.map(({ label, href }) => (
+        <Link
+          href={href}
+          key={label}
+          className={cn(pathname === href && "text-primary font-semibold")}
+        >
+          {label}
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+export default NavItems;
---
+++ apps/web/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+import NavItems from "./NavItems";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <Link href="/">
+        <div className="flex items-center gap-2.5 cursor-pointer">
+          <Image src="/images/logo.svg" alt="Logo" width={46} height={44} />
+        </div>
+      </Link>
+      <div className="flex items-center gap-8">
+        <NavItems />
+        <p>Sign In</p>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
