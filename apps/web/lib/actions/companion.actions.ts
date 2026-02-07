51e0e39e66f9253cd684244160d35c1cf1e9081b
xwaynex <waynexaint@gmail.com>
2026-02-07 18:18:06 +0000

    refactor: improve formatting and error handling in companion actions and Supabase client

--- apps/web/lib/actions/companion.actions.ts
+++ apps/web/lib/actions/companion.actions.ts
@@ -1,4 +1,4 @@
-'use server';
+"use server";
 
 import { auth } from "@clerk/nextjs/server";
 import { createSupabaseClient } from "../supabase";
@@ -16,4 +16,33 @@ export const createCompanion = async (formData: CreateCompanion) => {
     throw new Error(error?.message || "Failed to create companion");
 
   return data[0];
+};
+
+export const getAllCompanions = async ({
+  limit = 10,
+  subject,
+  topic,
+  page = 1,
+}: GetAllCompanions) => {
+  const supabase = createSupabaseClient();
+
+  let query = supabase.from("companions").select();
+
+  if (subject && topic) {
+    query = query
+      .ilike("subject", `%${subject}%`)
+      .or(`topic.ilike.%${topic}%,name.ilike.%${topic}%`);
+  } else if (subject) {
+    query = query.ilike("subject", `%${subject}%`);
+  } else if (topic) {
+    query = query.or(`topic.ilike.%${topic}%,name.ilike.%${topic}%`);
+  }
+
+  query = query.range((page -1) * limit, page * limit - 1);
+
+  const { data: companions, error } = await query;
+
+  if(error) throw new Error(error.message);
+
+  return companions;
 };
