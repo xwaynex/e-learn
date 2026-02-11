997ddeebed1c2adb934976a324b805c6223995cf
xwaynex <waynexaint@gmail.com>
2026-02-11 19:16:46 +0000

    feat: implement companion creation permissions and enhance UI for limit notification

--- apps/web/lib/actions/companion.actions.ts
+++ apps/web/lib/actions/companion.actions.ts
@@ -114,3 +114,33 @@ export const getUserCompanions = async (userId: string) => {
 
   return data;
 };
+
+export const newCompanionPermissions = async () => {
+  const { userId, has } = await auth();
+
+  const supabase = await createSupabaseClient();
+
+  let limit = 0;
+
+  if (has({ plan: "pro" })) {
+    return true;
+  } else if (has({ feature: "3_companion_limit" })) {
+    limit = 3;
+  } else if (has({ feature: "10_companion_limit" })) {
+    limit = 10;
+  }
+
+  const { data, error } = await supabase
+    .from("companions")
+    .select("id", { count: "exact" })
+    .eq("author", userId);
+
+  if (error) throw new Error(error.message);
+  const companionCount = data?.length;
+
+  if(companionCount >= limit) {
+    return false
+  } else  {
+    return true
+  }
+};
